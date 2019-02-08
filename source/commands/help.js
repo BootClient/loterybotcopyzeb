f5cde5558083db526972d51dc8686a6e7863b2e9
BootClient <45828302+BootClient@users.noreply.github.com>
2019-02-08 23:11:28 +0100

    Update help.js

--- source/commands/help.js
+++ source/commands/help.js
@@ -9,6 +9,6 @@ module.exports = class extends Command {
 
     execute(message) {
         //message.author.send("```mg!create <time-in-seconds> <giveaway-title>\nmg!delete <giveaway-id>```");
-        message.channel.send("**Salut tout le monde!** Je suis connu comme Vulnix. Giveaway. Bien que mes origines soient pour la plupart inconnues, il est connu que l'on m'associe à Zebulon™#666. Ha. Quels imbéciles. Dites-moi, cherchez-vous un cadeau automatisé? Allez-vous bien? J'ai la chose parfaite pour toi. Essayez `!ginvite` pour me mettre sur votre serveur, puis lancez-vous et lancez votre propre cadeau en utilisant les commandes de` !gcommandes`. Je te verrai bientôt ...");
+        message.channel.send("**Salut tout le monde!** Bien que mes origines soient pour la plupart inconnues, il est connu que l'on m'associe à WaynStalker ™ [ANON]#7777. Dites-moi, cherchez-vous un cadeau automatisé? Allez-vous bien? J'ai la chose parfaite pour toi. Essayez `!ginvite` pour me mettre sur votre serveur, puis lancez-vous et lancez votre propre cadeau en utilisant les commandes de` !gcommandes`. Je te verrai bientôt ...");
     }
 };
